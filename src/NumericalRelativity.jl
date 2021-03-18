8ca6ed8e06ce02b7ac4013a39f66e80fd9abe9ad
Markus Amano <markus.a.amano@gmail.com>
2021-03-18 10:12:19 -0500

    made a plan

---
+++ src/NumericalRelativity.jl
@@ -0,0 +1,19 @@
+module NumericalRelativity
+
+# Tensor
+#
+# (array of functions, coordinates, (indices up or down)) -> tensor
+# T[-1,-1] means lower-lower indices and T[1,1] means up-up
+# You should be able to find the Rank of tensors
+# Contracting is possible? At least with a function contract
+#
+# Metric should be a rank two tensor
+#
+# partial gives back the symbols ∂T
+# christoffel symbols should be (metric) -> christoffel
+#
+# Riemann Tensor g -> Riemann Tensor
+# Ricci Tensor g -> Ricci Tensor
+# Ricci Scalar g -> Ricci Scalar
+
+end
